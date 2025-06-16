9d782a3ad6690d3ab64576417b93b6f761274ab8
oscar <haojchung@gmail.com>
2025-06-16 13:39:13 +0800

    Use in-line svg to avoid `Suspense` wrapper from interfering with logo

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -18,7 +18,6 @@ import {
   Typography,
 } from "@mui/material";
 import Cookies from "js-cookie";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
@@ -224,13 +223,38 @@ export default function LoginForm({
 function AppLogo() {
   return (
     <Box sx={{ mb: 4, textAlign: "center" }}>
-      <Image
-        src="/globe.png"
-        alt="Merit System Logo"
-        width={80}
-        height={80}
-        priority
-      />
+      {/* Inline SVG to avoid loading issues */}
+      <Box
+        sx={{
+          width: 80,
+          height: 80,
+          display: "block",
+          margin: "0 auto",
+          mb: 1,
+        }}
+      >
+        <svg
+          width="80"
+          height="80"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+        >
+          <g clipPath="url(#a)">
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.27 14.1a6.5 6.5 0 0 0 3.67-3.45q-1.24.21-2.7.34-.31 1.83-.97 3.1M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.48-1.52a7 7 0 0 1-.96 0H7.5a4 4 0 0 1-.84-1.32q-.38-.89-.63-2.08a40 40 0 0 0 3.92 0q-.25 1.2-.63 2.08a4 4 0 0 1-.84 1.31zm2.94-4.76q1.66-.15 2.95-.43a7 7 0 0 0 0-2.58q-1.3-.27-2.95-.43a18 18 0 0 1 0 3.44m-1.27-3.54a17 17 0 0 1 0 3.64 39 39 0 0 1-4.3 0 17 17 0 0 1 0-3.64 39 39 0 0 1 4.3 0m1.1-1.17q1.45.13 2.69.34a6.5 6.5 0 0 0-3.67-3.44q.65 1.26.98 3.1M8.48 1.5l.01.02q.41.37.84 1.31.38.89.63 2.08a40 40 0 0 0-3.92 0q.25-1.2.63-2.08a4 4 0 0 1 .85-1.32 7 7 0 0 1 .96 0m-2.75.4a6.5 6.5 0 0 0-3.67 3.44 29 29 0 0 1 2.7-.34q.31-1.83.97-3.1M4.58 6.28q-1.66.16-2.95.43a7 7 0 0 0 0 2.58q1.3.27 2.95.43a18 18 0 0 1 0-3.44m.17 4.71q-1.45-.12-2.69-.34a6.5 6.5 0 0 0 3.67 3.44q-.65-1.27-.98-3.1"
+              fill="#1976d2"
+            />
+          </g>
+          <defs>
+            <clipPath id="a">
+              <path fill="#fff" d="M0 0h16v16H0z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </Box>
       <Typography variant="h4" component="h1" sx={{ mt: 2, fontWeight: 700 }}>
         Merit System
       </Typography>
