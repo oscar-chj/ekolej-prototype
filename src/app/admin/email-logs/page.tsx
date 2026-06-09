69dd2039cbc1bd6f5f2195c27e70065497748586
cy1129-tech <228636@student.upm.edu.my>
2026-06-09 23:04:04 +0800

    remove localStorage in pagge.tsx & change npm install to yarn (package)

--- src/app/admin/email-logs/page.tsx
+++ src/app/admin/email-logs/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-if (typeof window === 'undefined') {
-  (global as any).localStorage = { getItem: () => null, setItem: () => {} };
-}
 
 import React, { useState, useEffect, useTransition } from 'react';
 import { sendEmailAction } from "@/app/actions/emailAction";
